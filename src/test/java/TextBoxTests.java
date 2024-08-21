2d397713afb0a22a47cabe679ade7ca3699e9154
Daniil Zotov <danjke99@yandex.ru>
2024-08-21 22:59:31 +0300

    added TextBoxTests

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,20 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
-
+@BeforeAll
+static void BeforeAll(){
+    com.codeborne.selenide.Configuration.browserSize = "1920x1080";
+    Configuration.baseUrl = "https://demoqa.com";
+}
     @Test
     void fillFormTest(){
-        open("https://demoqa.com/text-box");
-        $("#userName").setValue("Alex Egorov");
-        $("#userEmail").setValue("alex@egorov.ru");
-        $("#currentAddress").setValue("Some address 1");
-        $("#permanentAddress").setValue("Another address 1");
-        $("#submit").click();
-        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.ru"),
+        open("/text-box");
+        $("[id=userName]").setValue("Alex Egorov");
+        $("[id=userEmail]").setValue("alex@egorov.ru");
+        $("[id=currentAddress]").setValue("Some address 1");
+        $("[id=permanentAddress]").setValue("Another address 1");
+        $("[id=submit]").click();
+        $("[id=output]").shouldHave(text("Alex Egorov"), text("alex@egorov.ru"),
                 text("Some address 1"), text("Another address 1"));
 
     }
